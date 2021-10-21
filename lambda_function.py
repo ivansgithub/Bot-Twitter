208dd03ebed3966fb6a7d3a2cb036003de7e95a4
Ivan Salazar <ivan.sal.be@gmail.com>
2021-10-21 11:05:09 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,14 +6,17 @@ import tweepy
 import re
 
 
-
+# include everything in a function to run in lambda aws
 
 def lamba_handle(event,context):
         
+    # make a request to the page to scrape    
     r=requests.get('http://www.bcra.gob.ar/PublicacionesEstadisticas/Informe_monetario_diario.asp')
         
+    # get the content of the html    
     dom = BS(r.content , features = 'html.parser')
         
+    #search for the element with the link we need    
     dom2=dom.find('ul',{'class':'post-pagina-interior'})
         
     dom3=dom2.find('a')
@@ -22,33 +25,38 @@ def lamba_handle(event,context):
         
     dom5='http://www.bcra.gob.ar'+dom4.replace('..','')
         
+    # make a second request to the page to scrape    
     rq = requests.get(dom5)
-
+    
+    #Download the text from the pdf online
     pdf = pdfplumber.load(BytesIO(rq.content))
     page = pdf.pages[3]
     page2 = pdf.pages[2]
     text=page.extract_text()
     text3=page2.extract_text()
     
-    
+    #text mining process
     text=text.split('\n')
     
     text3=text3.replace('\n','')
             
     text2=[i for i in text if 'Base monetaria' in i]
             
-    monto=text2[0].split('Base monetaria')[2]       
-    monto=monto.split(' ')
-    monto=monto[1].strip()
+    amount=text2[0].split('Base monetaria')[2]       
+    amount=amount.split(' ')
+    amount=amount[1].strip()
            
-    fecha=re.search(r'Informe Monetario Diario+\s+[0-9]{1,2}\s\w\w\s(?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)\s\w\w\s[0-9]{3,4}',text3)
-    fecha=fecha.group().replace('Informe Monetario Diario  ', '')
+    date=re.search(r'Informe Monetario Diario+\s+[0-9]{1,2}\s\w\w\s(?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)\s\w\w\s[0-9]{3,4}',text3)
+    date=date.group().replace('Informe Monetario Diario  ', '')
         
-    api_key='uy6CzZrOGBElppKiL9SJdqt8N'
-    api_key_secret='5NzPJUYGoBb7KsPbedYvatzssd7GA1VtepPv6VXGqK86mKFbUJ'
+    #access to twitter api    
         
-    access_token='1448023730173583361-whw7nPHiGugMsHxkFKAelWs1pBwH6V'
-    access_token_secret='EY4eNCYt6zy1vx1gfqqIPeAmeKxz6EsTMBlgS1VfiOWhb'
+        
+    api_key='Your_key'
+    api_key_secret='Your_key_secret'
+        
+    access_token='Your_token'
+    access_token_secret='Your_secret_token'
         
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token,access_token_secret)
@@ -56,10 +64,10 @@ def lamba_handle(event,context):
     api = tweepy.API(auth)
         
         
-        
+    #tweet format
     single_tweet='La variacion de la base moneraria el ' + fecha +'\n'+'fue de $'+ monto + ' millones '
     
-    
+    #delete a tweet with the same format if it exists
     for statuss in tweepy.Cursor(api.user_timeline).items():
       
         try:
@@ -67,6 +75,7 @@ def lamba_handle(event,context):
         except:
             pass
             
+    #post the twitt
     api.update_status(status=single_tweet)
 
 
